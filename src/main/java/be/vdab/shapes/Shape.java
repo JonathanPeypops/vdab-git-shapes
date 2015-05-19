d6de9d965aae0920012690c3f397d468b3fd4edd
Jonathan Peypops <jonathanpeypops@msn.com>
2015-05-19 09:35:27 +0200

    Triangle + Shapes updaded : added perimeter

--- src/main/java/be/vdab/shapes/Shape.java
+++ src/main/java/be/vdab/shapes/Shape.java
@@ -2,13 +2,18 @@ package be.vdab.shapes;
 
 public abstract class Shape {
     protected String name;
-
+    double perimeter;
+    
     public Shape(String name) {
         this.name = name;
+    }
+    
+    public double perimeter(){
+		return perimeter;
     }
 
     @Override
     public String toString() {
-        return "Shape '" + name + "'";
-    }
+        return "Shape '" + name + "' " + perimeter;
+    }  
 }
--- src/main/java/be/vdab/shapes/Triangle.java
+++ src/main/java/be/vdab/shapes/Triangle.java
@@ -3,5 +3,6 @@ package be.vdab.shapes;
 public class Triangle extends Shape {
     public Triangle(String name) {
         super(name);
+        perimeter = 16;
     }
 }
